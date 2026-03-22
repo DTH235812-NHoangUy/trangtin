e4ca633bc00246d25ba43c42b52d75649fb5008b
DTH235812-NHoangUy <uy_dth235812@student.agu.edu.vn>
2026-03-22 16:38:58 +0700

    Fix CKEditor config path

--- modules/firstimage.js
+++ modules/firstimage.js
@@ -1,8 +1,8 @@
 function firstImage(noiDung) {
-	var regExp = /<img[^>]+src="?([^"\s]+)"?[^>]*\/>/g;
+	var regExp = /<img[^>]+src="?([^"\s]+)"?[^>]*\/?>/i;
 	var results = regExp.exec(noiDung);
-	var image = 'http://127.0.0.1:3000/images/noimage.png';
-	if(results) image = results[1];
+	var image = '/images/noimage.png';
+	if (results) image = results[1];
 	return image;
 }
 
